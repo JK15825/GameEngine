afc63e9e13eef29d5524bd705c0d6c97219c8f44
DESKTOP-FV4IBUR\Jett Kaspar <baseballranger>
2017-08-29 16:20:21 -0500

    Created the screen class and started to implement the game loop in the run class.

--- src/runTime/Run.java
+++ src/runTime/Run.java
@@ -5,12 +5,53 @@
  */
 package runTime;
 
-public class Run {
+import javax.swing.JFrame;
+import javax.swing.JRootPane;
+import javax.swing.KeyStroke;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
 
-	public static void main(String[] args) 
+public class Run implements Runnable
+{
+	private JFrame frame;
+	private Screen gameScreen;
+	public Run()
 	{
-		System.out.println("Suck my ass");
+		frame = new JFrame("THE FRAME");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		gameScreen = new Screen();
+		frame.add(gameScreen);
+		
+		frame.pack();
+		frame.setSize(720,480);
+		frame.setResizable(false);
 
 	}
-
+	public void start()
+	{
+		Thread t = new Thread(this);
+		t.start();
+	}
+	public void run()
+	{
+		init();
+	}
+	private void init()
+	{
+		frame.setVisible(true);
+		JRootPane rootPane = frame.getRootPane();
+		rootPane.getInputMap(JRootPane.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("pressed ESC"), "closeWindow");
+		rootPane.getActionMap().put("closeWindow",new AbstractAction() {
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		});
+	}
+	
+	public static void main(String args[])
+	{
+		new Run().start();
+	}
 }
--- src/runTime/Screen.java
+++ src/runTime/Screen.java
@@ -1,5 +1,29 @@
 package runTime;
 
-public class Screen {
-
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import javax.swing.JPanel;
+public class Screen extends JPanel
+{
+	private int WIDTH;
+	private int HEIGHT;
+	
+	public Screen()
+	{
+		WIDTH = 720;
+		HEIGHT = 480;
+	}
+	
+	public Screen(int width, int height)
+	{
+		WIDTH = width;
+		HEIGHT = height;
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		if(g != null)
+			g.fillRect(0, 0, 500, 500);
+	}
 }
