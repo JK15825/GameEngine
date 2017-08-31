9e2b4ac63204e0dd9d15fb901ee36f2401d4c753
fatcat9898 <colin.j.wakefield@gmail.com>
2017-08-30 20:29:20 -0500

    stuff i realy dont rember

--- src/helperClasses/DefaultItem.java
+++ src/helperClasses/DefaultItem.java
@@ -2,10 +2,17 @@ package helperClasses;
 
 public abstract class DefaultItem 
 {
-	public Vector3D MovmentVector ; 
-	public Point3D Location ; 
-	public float Mass ;
+	public Vector3D vector ; 
+	public Point3D location ; 
+	public float mass ;
 	public abstract void Update() ; 
 	public abstract void Draw() ;
+	public float elasticity ; 
+	public float CaculateMomentum()
+	{
+		return vector.magnitude() * mass;
+	}
+	public abstract void colide(Vector3D u); 
+
 	
 }
--- src/helperClasses/Vector3D.java
+++ src/helperClasses/Vector3D.java
@@ -58,5 +58,18 @@ public class Vector3D
 		this.z *= scalar ; 
 
 	}
+	public float magnitude()
+	{
+		return (float) Math.sqrt(Math.pow(this.x, 2) +Math.pow(this.y, 2) +Math.pow(this.z, 2));
+
+	}
+	public float dotProduct(Vector3D v )
+	{
+	 return this.x * v.x + this.y * v.y + this.z * v.z;
+	}
+	public Vector3D crossproduct(Vector3D v )
+	{
+	 return  new Vector3D(this.y * v.z - v.y * this.z  , this.z * v.x - v.x * this.z , this.x * v.y - v.y * this.x); 
+	}
 	
 }
